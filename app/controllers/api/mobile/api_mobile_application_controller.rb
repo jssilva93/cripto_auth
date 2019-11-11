9c42e695ad486bef53eedefb8df5b2afd92fdaae
jssilva93 <jssilva93@gmail.com>
2019-11-10 20:20:07 -0500

    cors

--- app/controllers/api/mobile/api_mobile_application_controller.rb
+++ app/controllers/api/mobile/api_mobile_application_controller.rb
@@ -1,6 +1,7 @@
 class Api::Mobile::ApiMobileApplicationController < ApplicationController
   #include DetailedLogging
   before_action :set_locale#, :current_session, :current_user
+  skip_before_action :verify_authenticity_token
 
   def current_session
     @passenger_session ||= Session.where(session_token: params[:t]).first
